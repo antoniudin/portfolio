f1e695dd9ed0f56ae0d68a39c9dc146a110fabb6
Anton Iudin <yudin431@gmail.com>
2022-05-12 21:40:32 -0400

    deploy to the gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import test from '../src/style/test.sass'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="test">123</div>
+      <div className="title">Title</div>
+      <div className="menu">
+        <div className="title">Event</div>
+        <div className="title">Booking</div>
+      </div>
     </div>
   );
 }
